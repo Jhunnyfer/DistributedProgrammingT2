85231778ef3bff2d949f6d7f562a9e121a4e2864
jhumamc <jhumamc@gmail.com>
2019-09-29 10:30:38 -0500

    Agregar archivos de proyecto.

---
+++ DistributedPrism/DistributedPrism/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Prism;
+using Prism.Ioc;
+using DistributedPrism.ViewModels;
+using DistributedPrism.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace DistributedPrism
+{
+    public partial class App
+    {
+        /* 
+         * The Xamarin Forms XAML Previewer in Visual Studio uses System.Activator.CreateInstance.
+         * This imposes a limitation in which the App class must have a default constructor. 
+         * App(IPlatformInitializer initializer = null) cannot be handled by the Activator.
+         */
+        public App() : this(null) { }
+
+        public App(IPlatformInitializer initializer) : base(initializer) { }
+
+        protected override async void OnInitialized()
+        {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MTM3Njg0QDMxMzcyZTMyMmUzMGUvQlg3Tnk5ODRGQ01pbzNnWmEyWHdWcExaaUVOQ0FKODZGNDFpekRtd2M9");
+
+            InitializeComponent();
+
+            await NavigationService.NavigateAsync("NavigationPage/MainPage");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
+        }
+    }
+}
---
+++ DistributedProgrammingT2.Common/Models/Response.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DistributedProgrammingT2.Common.Models
+{
+    public class Response<T> where T : class
+    {
+        public bool IsSuccess{ get; set; }
+
+        public string Message { get; set; }
+
+        public T Result { get; set; }
+    }
+}
